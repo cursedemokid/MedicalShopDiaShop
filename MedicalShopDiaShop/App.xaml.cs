870f890ee946ceb86b8c5e4059f076a07acc3d72
Елизавета <navialex7@gmail.com>
2026-04-13 15:55:26 +0300

    Для любимки

--- MedicalShopDiaShop/App.xaml.cs
+++ MedicalShopDiaShop/App.xaml.cs
@@ -16,7 +16,7 @@ namespace MedicalShopDiaShop
     /// </summary>
     public partial class App : Application
     {
-        public static DiaShopEntities context = new DiaShopEntities();
+        public static DiaShopEntities1 context = new DiaShopEntities1();
         public static Database.User currentUser = new Database.User();
         //public static MedicalShopDiaShopEntities1 context = new MedicalShopDiaShopEntities1();
         public static Button currentButton = new Button();
